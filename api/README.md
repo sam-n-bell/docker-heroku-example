007802d4db42b0733dcfe4051ee455226675be8e
Sam Bell <bell137@gmail.com>
2021-05-20 21:25:03 -0500

    notes after it worked

--- api/README.md
+++ api/README.md
@@ -10,3 +10,13 @@
 # windows (first time)
 # run powershell as admin, paste in: Set-ExecutionPolicy Unrestricted -Force
 # press enter
+
+
+# Heroku things
+1. heroku login
+2. git init
+2. heroku create <app-name>
+3. git add .
+4. git commit things
+5. heroku stach:set container <--- tells heroku your app is ran within a container
+6. git push (to heroku!)
--- api/app.py
+++ api/app.py
@@ -8,6 +8,10 @@ def hello():
   return {"secret": "not very secret"}
 
 if __name__ == '__main__':
+  # Heroku runs your app depending on the ports available. 
+  # In several instances, It might not assign the port 5000 which means our app crashes. 
+  # In order to fix this error, we’d add this to our “app.py”
+  # This tells our app to use the port assigned by heroku, if not, use 5000.
   port = int(os.environ.get('PORT', 5000))
   print(f'Running on port {port} {type(port)}')
   app.run(host = '0.0.0.0', port = port)
